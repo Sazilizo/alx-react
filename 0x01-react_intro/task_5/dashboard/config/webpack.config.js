8d689ae40d5b6d85691a9fee9e257db8a9e65216
unknown <lizosazi@gmail.com>
2024-06-04 21:59:36 +0200

    installing babel

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -5,7 +5,7 @@ module.exports ={
     entry:path.resolve(__dirname, "../src/index.js"),
     output: {
         filename: 'bundle.js', // Set the output filename to bundle.js
-        path: path.resolve(__dirname,'../dist'),
+        path: path.resolve(__dirname,'./public'),
     },
     module:{
         rules:[
@@ -15,15 +15,15 @@ module.exports ={
             },
             {
                 test: /\.(png|jpe?g|svg|gif)$/i,
-                use :"image-webpack-loader"
+                use :"file-loader"
             },
             {
-                test: /\.js$/,
+                test: /\.(jsx|js)$/,
                 exclude: /node_modules/,
                 use: {
                   loader: 'babel-loader',
                   options: {
-                    presets: ['@babel/preset-env'],
+                    "presets": [ "@babel/preset-env", "@babel/preset-react" ],
                   },
                 },
             },
