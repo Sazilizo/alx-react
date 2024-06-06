866f7a98bea850180dd1711756602a898dc51ec0
unknown <lizosazi@gmail.com>
2024-06-06 22:06:06 +0200

    added style-loader

---
+++ 0x01-react_intro/task_5/dashboard/dist/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const h2 = document.createElement("h2");
+h2.innerHTML = "Hello world of webpack and react";
+document.body.appendChild(h2);
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsTUFBTUEsRUFBRSxHQUFHQyxRQUFRLENBQUNDLGFBQWEsQ0FBQyxJQUFJLENBQUM7QUFDdkNGLEVBQUUsQ0FBQ0csU0FBUyxHQUFFLGtDQUFrQztBQUVoREYsUUFBUSxDQUFDRyxJQUFJLENBQUNDLFdBQVcsQ0FBQ0wsRUFBRSxDQUFDLEMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9kYXNoYm9hcmQvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgaDIgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KFwiaDJcIik7XHJcbmgyLmlubmVySFRNTD0gXCJIZWxsbyB3b3JsZCBvZiB3ZWJwYWNrIGFuZCByZWFjdFwiO1xyXG5cclxuZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChoMik7Il0sIm5hbWVzIjpbImgyIiwiZG9jdW1lbnQiLCJjcmVhdGVFbGVtZW50IiwiaW5uZXJIVE1MIiwiYm9keSIsImFwcGVuZENoaWxkIl0sInNvdXJjZVJvb3QiOiIifQ==
