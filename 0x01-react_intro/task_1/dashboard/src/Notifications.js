84492065e9109adde9199800f9c82551a6143768
unknown <lizosazi@gmail.com>
2024-06-06 14:25:52 +0200

    embedding expressions and functions

---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export function Notifications(){
+    return (
+        <div className="Notifications">
+            <p>Here is the list of notifications</p>
+        </div>
+    );
+}
