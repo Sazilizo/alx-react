af66cd89c59e1ccfe69f8b96a025b9c9f16f5c36
unknown <lizosazi@gmail.com>
2024-06-06 22:01:42 +0200

    added style-loader

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                use: ["style-loader", "css-loader"],
             },
             {
                 test: /\.(png|jpg|jpeg|gif|svg)$/i,
