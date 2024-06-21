2416708ad6530a628a0e97c8e11da5116d753315
unknown <lizosazi@gmail.com>
2024-06-21 17:07:20 +0200

    updating the course list and toggling with props

---
+++ 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function CourseListRow({isHeader=false, textFirstCell, textSecondCell=null}) {
+  return (
+    <tr>
+        {
+            isHeader ? (
+                <>
+                {
+                    textSecondCell ? (
+                        <>
+                            <th>{textFirstCell}</th>
+                            <th>{textSecondCell}</th>
+                        </>
+                        ) : (
+                            <th colSpan="2">{textFirstCell}</th>
+                    )
+                }
+                </>
+            ) : (
+                <>
+                <td>{textFirstCell}</td>
+                <td>{textSecondCell}</td>
+                </>
+            )
+        }
+    </tr>
+  )
+}
+
+export default CourseListRow
