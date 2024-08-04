750276414e7b78fbbe386e921e76bc177df32d11
unknown <lizosazi@gmail.com>
2024-08-04 22:28:02 +0200

    fixed issues with 4-mutations.js

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-export const people = Map({
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -9,6 +9,6 @@ export const people = Map({
   6: 'Lucas'
 });
 
-export const map = people.withMutations((item) => {
+export const map2 = map.withMutations((item) => {
   item.set(2, 'Benjamin').set(4, 'Oliver');
 });
